5a729e59651d8d46a46f34f19439f0f179129e03
Devroy Blake <necrodevz@gmail.com>
2016-11-13 18:03:41 -0800

    init

---
+++ src/components/Application/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import Ajax from 'react-ajax';
+
+class Application extends Component {
+	constructor(props) {
+		super(props);
+	}
+	
+}
+
+export default Application;
